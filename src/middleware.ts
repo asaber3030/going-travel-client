87f07cd06ca16b0ee4df6fbdb864e1295884e624
asaber3030 <abdulrahmansaber70@gmail.com>
2025-04-16 18:40:14 +0200

    remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,21 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { getUser } from "./actions/auth";
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+import { getUser } from "./actions/auth"
 
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl
 
-  // Exclude the login page from authentication check
+  /*   // Exclude the login page from authentication check
   if (pathname.startsWith("/admin") && pathname !== "/admin/login") {
     const user = await getUser();
     if (!user) {
       return NextResponse.redirect(new URL("/admin/login", request.url));
     }
-  }
+  } */
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
   matcher: ["/admin/:path*"]
-};
+}
