c681294d7461ffaa7f5d81b00d43aa2d82ed8c25
asaber3030 <abdulrahmansaber70@gmail.com>
2025-04-29 20:59:20 +0300

    add changes

--- src/app/(app)/hotels/_actions/data.ts
+++ src/app/(app)/hotels/_actions/data.ts
@@ -1,26 +1,26 @@
-"use server";
+"use server"
 
-import { getRequest } from "@/lib/axios";
-import { build } from "search-params";
+import { getRequest } from "@/lib/axios"
+import { build } from "search-params"
 
-import { UIHotel } from "@/types/ui";
-import { PaginatedData } from "@/types";
+import { UIHotel } from "@/types/ui"
+import { PaginatedData } from "@/types"
 
 export async function getUIHotels(sp: Record<string, string | number> = { take: 12 }): Promise<PaginatedData<UIHotel>> {
   try {
-    const params = build(sp);
-    const response = await getRequest<PaginatedData<UIHotel>>(`/ui/hotels?${params}`);
-    return response.data;
+    const params = build(sp)
+    const response = await getRequest<PaginatedData<UIHotel>>(`/ui/hotels?${params}`)
+    return response.data
   } catch (error) {
-    throw new Error("Failed to fetch limousines");
+    throw new Error("Failed to fetch limousines")
   }
 }
 
 export async function getUIHotelById(id: number): Promise<UIHotel> {
   try {
-    const response = await getRequest<UIHotel>(`/ui/hotels/${id}`);
-    return response.data;
+    const response = await getRequest<UIHotel>(`/ui/hotels/${id}`)
+    return response.data
   } catch (error) {
-    throw new Error(`Failed to fetch limousine by ID: ${id}`);
+    throw new Error(`Failed to fetch limousine by ID: ${id}`)
   }
 }
